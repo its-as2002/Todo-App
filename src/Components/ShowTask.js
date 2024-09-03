2ce61fea34d8250e08cd2cbeb1b6f4373dca076c
Ayush Srivastava <its.as2002@gmail.com>
2024-09-03 23:56:27 +0530

    fix-alignment issue

--- src/Components/ShowTask.js
+++ src/Components/ShowTask.js
@@ -1,22 +1,30 @@
-import Task  from "./Task";
+import Task from "./Task";
 const ShowTask = (props) => {
-	const { taskList,deleteTask } = props;
-	
+	const { taskList, deleteTask } = props;
 
 	return (
 		<div className="flex flex-col">
-			<div className="bg-cyan-500 flex p-4 justify-evenly">
-				<div className="p-6 text-white font-bold ">No.</div>
-				<div className="p-4">
-					<h1 className="text-white font-bold p-2">Task Name</h1>
+			<div className="bg-cyan-500 flex w-full">
+				<div className="py-4 w-[10%] text-white font-bold text-center ">
+					No.
 				</div>
-				<div className="p-4">
-					<h1 className="text-white font-bold p-2">Task Description</h1>
-				</div>
+
+				<h1 className="text-white font-bold py-4 w-[20%]  text-center">
+					Task Name
+				</h1>
+
+				<h1 className="text-white font-bold py-4 w-[60%] text-center">
+					Task Description
+				</h1>
 			</div>
 			<div>
-				{taskList.map((taskData,index) => (
-					<Task data={taskData} deleteTask={deleteTask} index={index} key={taskData.id} />
+				{taskList.map((taskData, index) => (
+					<Task
+						data={taskData}
+						deleteTask={deleteTask}
+						index={index}
+						key={taskData.id}
+					/>
 				))}
 			</div>
 		</div>
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,12 +1,12 @@
 const Task = ({ data, deleteTask, index }) => {
 	const { taskName, taskDescription, id } = data;
 	return (
-		<div className="flex w-full my-2">
-			<h1 className="w-[10%] text-center bg-slate-500">{index + 1}</h1>
+		<div className="flex w-full my-2 ">
+			<h1 className="w-[10%] text-center text-white bg-slate-500">{index + 1}</h1>
 			<h1 className="w-[30%] text-center bg-slate-300">{taskName}</h1>
-			<h1 className="w-[60%] bg-slate-500 text-center">{taskDescription}</h1>
+			<h1 className="w-[60%] bg-slate-500 text-white text-center">{taskDescription}</h1>
 			<button
-				className="w-[10%] text-"
+				className="w-[10%] text-center"
 				type="button"
 				onClick={() => {
 					deleteTask(id);
