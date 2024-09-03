13b8177809c3dc982c81d923c3ee1bc49066fc58
Ayush Srivastava <its.as2002@gmail.com>
2024-09-03 14:12:51 +0530

    fix: deploy error

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const App = () => {
-    return (
-        <div className="flex flex-col min-h-screen overflow-hidden">
-            <Header />
-            <div className="flex-grow overflow-y-auto">
-                <Body />
-            </div>
-            <Footer />
-        </div>
-    );
+	return (
+		<div className="flex flex-col min-h-screen overflow-hidden">
+			<Header />
+			<div className="flex-grow overflow-y-auto">
+				<Body />
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 const appRouter = createBrowserRouter([
-    {
-        path:'/',
-        element : <App />
-    }
-])
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
-const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(<RouterProvider router={appRouter}/>);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<RouterProvider router={appRouter} />);
